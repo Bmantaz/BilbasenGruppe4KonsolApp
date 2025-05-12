c6d11011b994ce76177e1cd6963a8a433fa1f15b
Bmantaz <Bjarkepa@gmail.com>
2025-05-12 15:20:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BilbasenGruppe4KonsolApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
